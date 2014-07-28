e0bb30bb5b4ac852d555a8864e8181932c95233b
Dustin Lewis <d.lew23@gmail.com>
2014-07-27 22:23:53 -0700

    adds README, passes first spec, single digit numbers

---
+++ README.md
@@ -0,0 +1,3 @@
+Ruby TDD assessment - this program converts an inputted number (1, 2, 32, etc.) into Roman numerals.
+
+-Dustin Lewis, 7/27/14
--- lib/roman_numeral_assess.rb
+++ lib/roman_numeral_assess.rb
@@ -1,1 +1,21 @@
-def roman_func = 
+def roman_func (number)
+	ones = [ "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+	tens = [ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+	hundreds = [ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+	thousands = [ "", "M", "MM", "MMM"]
+
+	numeral_result = []
+
+	convert = number.to_s.reverse.split(" ")
+	numeral_result.push(ones[convert[0].to_i])
+
+	numeral_result.join("").reverse
+
+	# puts numeral_result
+
+end
+
+# puts roman_func(5)
+
+	#input 29
+	# ("9 2")
